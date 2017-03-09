84e345a2dd34f4f776d09d8e2a0852dc74f427a5
Simone Mosciatti <simone@redbeardlab.tech>
2017-03-09 21:05:00 +0100

    start removing most common words

--- main.R
+++ main.R
@@ -1,3 +1,5 @@
+require(readr)
+
 require(httr)
 
 require(tidyr)
@@ -40,12 +42,15 @@ populate_comments <- function(kids){
 
 comments_freelance <- populate_comments(freelance_march_17_item$kids)
 comments_who_is_hiring <- populate_comments(who_is_hiring_march_17_item$kids)
-hiring <- tibble::tibble()
 
+common_words <- readr::read_csv("common_english.txt")
+to_remove <- readr::read_csv("to_remove.txt")
 
-word_count <- comments %>%
+word_count <- comments_freelance %>%
   tidytext::unnest_tokens(words, text) %>%
   dplyr::select(id, words) %>%
+  dplyr::filter(!(words %in% common_words$common)) %>%
+  dplyr::filter(!(words %in% to_remove$`to remove`)) %>%
   dplyr::group_by(id, words) %>%
   summarise(n = n())
 
