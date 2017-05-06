48e7151025d46ace0a7cfeef598dd055b00ce0be
EmanuelaFurfaro <emanuela.furfaro@quantide.com>
2017-05-06 19:10:22 +0200

    string manipulation

--- main.R
+++ main.R
@@ -1,22 +1,26 @@
-require(readr)
-
+# load required packages
+require(tidyverse)
 require(httr)
-
-require(tidyr)
-require(dplyr)
-
 require(tidytext)
+require(stringr)
 
+# import data to be used
+common_words <- readr::read_csv("common_english.txt")
+to_remove <- readr::read_csv("to_remove.txt")
+
+# set the website
 HN_API_url <- "https://hacker-news.firebaseio.com/v0/"
 
 freelance_march_17 <- "13764729"
 who_is_hiring_march_17 <- "13764728"
 
-get_item_url <- function(id){
+get_item_url <- function(id) {
   paste(HN_API_url, "item/", id, ".json", sep = "")
 }
 
-freelance_march_17_item <- httr::content( httr::GET( get_item_url(freelance_march_17) ) )
+freelance_march_17_item <- httr::content(httr::GET( get_item_url(freelance_march_17) ) )
+# we have 138 kids
+
 who_is_hiring_march_17_item <- httr::content( httr::GET( get_item_url(who_is_hiring_march_17) ) )
 
 populate_comments <- function(kids){
@@ -40,18 +44,58 @@ populate_comments <- function(kids){
   comments
 }
 
-comments_freelance <- populate_comments(freelance_march_17_item$kids)
-comments_who_is_hiring <- populate_comments(who_is_hiring_march_17_item$kids)
+comments_freelance <- populate_comments(freelance_march_17_item$kids) %>%
+  mutate_all(tolower) 
 
-common_words <- readr::read_csv("common_english.txt")
-to_remove <- readr::read_csv("to_remove.txt")
+comments_freelance <- comments_freelance %>%
+  mutate(id = as.integer(id))
 
+# there are warnings due to missing values. Let us see them:
+
+comments_all <- as_tibble(unlist(freelance_march_17_item$kids))
+names(comments_all) <- "id"
+
+# some comments are left out
+
+setdiff(comments_all, comments_freelance[,2])
+
+# the reason is not as clear to me: sometimes it is ok because they were either
+# deleted or they were empty but other times I do not know
+
+# who is really seeking?
+comments_freelance %>% 
+  dplyr::mutate(seeking = stringr::str_detect(text, regex("seeking work"))) %>% View()
+
+# get rid of useless text
+comments_freelance$text <- stringr::str_replace(comments_freelance$text, "\n", 
+                                                " ")
+# harmonise backend
+comments_freelance$text <- stringr::str_replace(comments_freelance$text, "beck", " back ")
+comments_freelance$text <- stringr::str_replace(comments_freelance$text, "back-end", 
+                                                "backend")
+comments_freelance$text <- stringr::str_replace(comments_freelance$text, "back end", 
+                                                "backend")
+comments_freelance$text <- stringr::str_replace(comments_freelance$text, "backends", 
+                                                "backend")
+comments_freelance$text <- stringr::str_replace(comments_freelance$text, "backed", 
+                                                "backend")
+
+# harmonise frontend
+comments_freelance$text <- stringr::str_replace(comments_freelance$text, "front ", 
+                                                "frontend")
+
+# check I have done it right
+comments_freelance %>% 
+  filter(stringr::str_detect(text, regex(" front"))) %>% View()
+
+
+# tidy text
 word_count <- comments_freelance %>%
   tidytext::unnest_tokens(words, text) %>%
-  dplyr::select(id, words) %>%
+  dplyr::select(id_user = id, words) %>%
   dplyr::filter(!(words %in% common_words$common)) %>%
   dplyr::filter(!(words %in% to_remove$`to remove`)) %>%
-  dplyr::group_by(id, words) %>%
+  dplyr::group_by(id_user, words) %>%
   summarise(n = n())
 
 total_words <- word_count %>%
@@ -59,3 +103,12 @@ total_words <- word_count %>%
   dplyr::distinct(words)
 
 words_by_id <- tidyr::spread(word_count, words, n)
+
+words_by_id %>% select(starts_with("front"))
+
+words_by_id %>% select(starts_with("back"))
+
+
+#comments_who_is_hiring <- populate_comments(who_is_hiring_march_17_item$kids)
+
+
